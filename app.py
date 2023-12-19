b8bd1cdb95299e1fbe4f70146b31a33b85480a27
keilany <keilanydes@gmail.com>
2023-12-19 19:48:57 -0300

    Acrescentando um caso de teste

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import requests
 
+"""EXEMPLO
+api_url = "https://api.example.com/data"
+result = processar_dados(api_url)
+
+print(result)
+"""
+
+"""Função que envia uma solicitação GET para api_url e recuperar dados"""
 def obter_dados_API(api_url):
     response = requests.get(api_url)
     return response.json() if response.ok else None
 
+"""Chama a função de obter dados e verifica, 
+se os dados forem obtidos, ele retorna a mensagem 
+com o dados processados, 
+se houver erro, ou a recuperação não for bem sucedida ele retornará uma mensagem de erro."""
 def processar_dados(api_url):
     data = obter_dados_API(api_url)
     if data:
--- test_app.py
+++ test_app.py
@@ -1,26 +1,36 @@
-
 import pytest
 from unittest.mock import Mock
 from app import processar_dados
 
+"""Fixture para criar uma simulação para a função obter_dados_API
+ e define seu valor de retorno como {"message": "Hello, world!"}."""
 @pytest.fixture
 def mock_obter_dados_API(mocker):
     # Criar um mock para a função obter_dados_API
-    return mocker.patch("app.obter_dados_API", return_value={"message": "Hello, world!"})
+    return mocker.patch("app.obter_dados_API", return_value={"message": "Testes de Mock!"})
 
+
+"""Casos de Teste"""
+
+"""1 - Chama a função processar_dados com uma URL de API falsa 
+e verifica se o resultado é o esperado. 
+Espera-se que o resultado seja "Processed data: Hello, world!"""
 def testar_dados_processo_API_com_dados(mock_obter_dados_API):
-    # Chamar a função processar_dados com o mock
     result = processar_dados("http://fake-api.com")
+    assert result == "Processed data: Testes de Mock!"
+    
 
-    # Verificar se o resultado é o esperado
-    assert result == "Processed data: Hello, world!"
-
+"""2 - Simula uma resposta de API malsucedida definindo seu valor
+de retorno como None, teste para verificar se não há nenhum dado."""
 def testar_dados_processo_API_sem_dados(mock_obter_dados_API):
-    # Configurar o mock para retornar None, simulando uma resposta de API sem sucesso
     mock_obter_dados_API.return_value = None
-
-    # Chamar a função processar_dados com o mock
     result = processar_dados("http://fake-api.com")
-
-    # Verificar se o resultado é o esperado
     assert result == "Error processing data"
+    
+
+"""3 - Para Mensagem Vazia:"""
+def testar_mensagem_vazia(mock_obter_dados_API):
+       mock_obter_dados_API.return_value = {"message": ""}
+       result = processar_dados("http://fake-api.com")
+       assert result == "Processed data: "
+       
